64d52058170393ce66c6d11735541deee0f2f652
lupetyman <lupetyman@gmail.com>
2018-08-18 12:03:14 -0400

    Build require_login method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  def require_login
+    #return head(:forbidden) unless session.include? :user_id
+    redirect_to root_path unless session.include? :user_id
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_log_in, only: [:show]
+  before_action :require_login, only: [:show]
 
   def show
     @user = User.find(session[:user_id])
