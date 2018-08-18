4e2b227ab5d6019a789cfc04597792a02b47fe79
lupetyman <lupetyman@gmail.com>
2018-08-18 09:16:18 -0400

    Build relationships in models

--- app/models/production.rb
+++ app/models/production.rb
@@ -1,2 +1,4 @@
 class Production < ApplicationRecord
+  has_many :expenses
+  has_many :users, through: :expenses
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :expenses
+  has_many :productions, through: :expenses 
+
+  has_secure_password
 end
