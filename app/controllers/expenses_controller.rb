04f2fc49bced2b4b3207eab3e30245c94cbba47e
lupetyman <lupetyman@gmail.com>
2018-08-18 12:17:52 -0400

    Build new expense form and action

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,38 @@
 class ExpensesController < ApplicationController
+  before_action :require_login
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @expense = Expense.new
+  end
+
+  def create
+    @expense = Expense.new(expense_params)
+    if @expense.save
+      redirect_to expense_path(@expense)
+    else
+      render :new
+    end 
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:vendor, :date, :location, :department, :user_id, :production_id, :total)
+  end
+
 end
