182d1993eba7457873947d606a50be5763229c0c
lupetyman <lupetyman@gmail.com>
2018-08-18 09:14:49 -0400

    Add password_digest to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_18_130813) do
+ActiveRecord::Schema.define(version: 2018_08_18_131405) do
 
   create_table "expenses", force: :cascade do |t|
     t.integer "amount"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2018_08_18_130813) do
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
