8583b952c7e762fc6a86770cc1a335ea05212ed7
lupetyman <lupetyman@gmail.com>
2018-08-18 11:41:47 -0400

    Add login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :edit, :update, :show]
   resources :expenses
 
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  get "/logout" => "sessions#destroy"
+
   root "welcome#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
