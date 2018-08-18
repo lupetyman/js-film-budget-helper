8c9635224709630aaa12c17125dc0cd46c6af44b
lupetyman <lupetyman@gmail.com>
2018-08-18 09:57:53 -0400

    Build new user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,21 @@
 class UsersController < ApplicationController
 
   def show
-  end 
+    @user = User.find()
+  end
 
   def new
+    @user = User.new
   end
 
   def create
-  end
-
-  def edit
-  end
-
-  def update
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
   end
 
 end
