29a6e54ddc9acf59aeb84f83836feee4ed5189f2
lupetyman <lupetyman@gmail.com>
2018-08-19 17:27:03 -0400

    Build view and action for new production

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -1,19 +1,24 @@
 class ProductionsController < ApplicationController
 
+  def index
+  end
+
   def show
   end
 
   def new
+    @production = Production.new
   end
 
   def create
+    raise params.inspect
   end
 
   def edit
   end
 
   def update
-  end 
+  end
 
 
 end
