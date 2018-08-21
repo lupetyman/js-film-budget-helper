f55cefbd512c26804a90ab7398839047bcdc8173
lupetyman <lupetyman@gmail.com>
2018-08-21 07:55:11 -0400

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :expenses
-  has_many :productions, through: :expenses 
+  has_many :productions, through: :expenses
+  validates :email, uniqueness: true
+  validates :role, presence: true
 
   has_secure_password
 end
