3e1593f472aa1ef2762c3920ae508de1697ed389
lupetyman <lupetyman@gmail.com>
2018-08-21 12:47:18 -0400

    Add uid column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_013906) do
+ActiveRecord::Schema.define(version: 2018_08_21_164607) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2018_08_21_013906) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "uid"
   end
 
 end
