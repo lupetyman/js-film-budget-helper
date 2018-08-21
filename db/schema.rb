37b2562188a391f1a721a670dbdab456f5bbe69d
lupetyman <lupetyman@gmail.com>
2018-08-21 13:26:54 -0400

    Remove role from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_164607) do
+ActiveRecord::Schema.define(version: 2018_08_21_172343) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -62,7 +62,6 @@ ActiveRecord::Schema.define(version: 2018_08_21_164607) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
