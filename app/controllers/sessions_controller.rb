4cc8a72616dc5bd462ac8e92983c273a4a83430d
lupetyman <lupetyman@gmail.com>
2018-08-21 13:26:16 -0400

    Fix validations to accommodate Omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,35 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:user][:email])
-    if @user && @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
+    if auth_hash
+      user = User.find_or_create_by(uid: auth_hash["uid"]) do |u|
+        u.uid = auth_hash["uid"]
+        u.name = auth_hash["info"]["name"]
+        u.email = auth_hash["info"]["email"]
+        u.password = SecureRandom.hex
+      end
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
     else
-      redirect_to login_path
+      user = User.find_by(email: params[:user][:email])
+      if user && user.authenticate(params[:user][:password])
+        session[:user_id] = user.id
+        redirect_to user_path(user)
+      else
+        render :new
+      end
     end
   end
 
   def destroy
     session.delete :user_id
     redirect_to root_path
+  end
+
+  private
+
+  def auth_hash
+    request.env["omniauth.auth"]
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
   has_many :expenses
   has_many :productions, through: :expenses
-  validates :email, uniqueness: true
-  validates :role, presence: true
   validates :name, presence: true
-  
+  validates :email, presence: true
+
 
   has_secure_password
 end
