582ab6aa3faaa46ce8359f662345345d573f9281
lupetyman <lupetyman@gmail.com>
2018-08-21 14:22:25 -0400

    Add errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
       user = User.find_by(email: params[:user][:email])
       if user && user.authenticate(params[:user][:password])
         session[:user_id] = user.id
-        redirect_to user_path(user)
+        redirect_to user_path(user.id)
       else
         render :new
       end
