1779d3ddbc66ab3f1cfaa0f213f24bcaaf0edaec
lupetyman <lupetyman@gmail.com>
2018-08-22 10:30:03 -0400

    Organize by Rails Style Guide standards

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   has_many :expenses
   has_many :productions, through: :expenses
+
   validates :name, presence: true
   validates :email, presence: true
-
 
   has_secure_password
 end
