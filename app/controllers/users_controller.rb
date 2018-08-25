f3d348a83a34d74137f96f9177a812fc2994a598
lupetyman <lupetyman@gmail.com>
2018-08-25 16:04:32 -0400

    Move call to Production model to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :require_login, only: [:show]
 
   def show
+    @productions = Production.all if current_user.admin
   end
 
   def new
