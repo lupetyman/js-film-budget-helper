bb3a2b9018c15770e16b4cf2af4802733e11ab9b
lupetyman <lupetyman@gmail.com>
2018-08-25 13:18:46 -0400

    Refactor setting user in sessions to private method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,11 @@ class SessionsController < ApplicationController
   def create
     if auth_hash
       @user = User.find_or_create_by_omniauth(auth_hash)
-      session[:user_id] = @user.id
-      redirect_to user_path(@user.id)
+      set_session_user(@user)
     else
       @user = User.find_by(email: params[:user][:email])
       if @user && @user.authenticate(params[:user][:password])
-        session[:user_id] = @user.id
-        redirect_to user_path(@user.id)
+        set_session_user(@user)
       else
         flash.now[:notice] = "Invalid email/password combination."
         @user = User.new
@@ -28,6 +26,11 @@ class SessionsController < ApplicationController
   end
 
   private
+
+  def set_session_user(user)
+    session[:user_id] = user.id
+    redirect_to user_path(user.id)
+  end
 
   def auth_hash
     request.env["omniauth.auth"]
