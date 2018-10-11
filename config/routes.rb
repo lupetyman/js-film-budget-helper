dff739a6d6b658827a2ccb5223e58b3a757f4487
lupetyman <lupetyman@gmail.com>
2018-10-11 17:43:51 -0400

    Create unnested expenses show route

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
     resources :expenses, only: [:show, :new, :create]
   end
 
-  resources :expenses, only: [:update, :destroy]
+  resources :expenses, only: [:show, :update, :destroy]
 
   get 'pending_expenses', to: 'expenses#pending'
   get 'approved_expenses', to: 'expenses#approved'
