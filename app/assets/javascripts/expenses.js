0e77a1787ca21769f8068ed653d5e1f60897664a
lupetyman <lupetyman@gmail.com>
2018-10-12 20:59:21 -0400

    Add makeTable() function to generate html table

--- app/assets/javascripts/expenses.js
+++ app/assets/javascripts/expenses.js
@@ -13,6 +13,7 @@ const attachListeners = () => {
 }
 
 const displayExpenses = () => {
+  makeTable(["Vendor", "Date", "Total"])
   const userId = $("h1").data("id")
   console.log(userId)
   //$("#user-expenses").html("<p>Hello</p>")
@@ -27,3 +28,25 @@ const showExpense = (expense) => {
   })
   alert("click")
 }
+
+const makeTable = (headers) => {
+  const table = "<table class="table"><tr>"
+  headers.forEach((header) => {
+    table += `<td><strong>${header}</strong></td>`
+  })
+  table += "</tr></table>"
+}
+
+
+    <% expenses.find_each do |expense| %>
+      <tr>
+        <td><%= expense.vendor %></td>
+        <td><%= expense.formatted_date %></td>
+        <td><%= expense.department_category %></td>
+        <td><%= expense.dollar_total %></td>
+        <td><%= expense.status %></td>
+        <td><a href="#" data-id="<%= expense.id %>" class="show-expense">See More</a></td>
+      </tr>
+      <div id="display-expense-<%= expense.id %>"></div>
+    <% end %>
+  </table>
