508e1bdf57bf0f2b747fcc5df210056177c5e17b
lupetyman <lupetyman@gmail.com>
2018-10-12 20:22:22 -0400

    Add functionality to get an individual expense

--- app/assets/javascripts/expenses.js
+++ app/assets/javascripts/expenses.js
@@ -8,9 +8,12 @@ const attachListeners = () => {
   })
 }
 
+// incomplete, would like to set up index before continuing
 const showExpense = (expense) => {
   let expenseId = $(expense).data("id")
+  let divId = "display-expense-" + expenseId
   $.get(`/expenses/${expenseId}`, function(data) {
-    console.log(data)
+    $(divId).window(data)
   })
+  alert("click")
 }
