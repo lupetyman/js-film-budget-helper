7f34b9b6fb8eaea4cb3981d97d217ce7e66eafe9
lupetyman <lupetyman@gmail.com>
2018-10-14 19:44:55 -0400

    Adjust create action to build, then save

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -31,19 +31,9 @@ class ExpensesController < ApplicationController
   end
 
   def create
-    @expense = current_user.expenses.create(expense_params)
+    @expense = current_user.expenses.build(expense_params)
+    @expense.save
     render json: @expense
-  #   respond_to do |f|
-  #     f.html
-  #     f.json {render json: @expense, status: 201}
-  #   end
-  #   # if @expense.save
-  #   #   @expense.receipt.attach(expense_params[:receipt])
-  #
-  #   #   #redirect_to user_expense_path(user_id: current_user.id, id: @expense.id)
-  #   # else
-  #   #   render :new
-  #   # end
   end
 
   def edit
